a3e39f63eaa0d82145e410b1612aeb9c46c9dd8c
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2023-10-22 16:58:14 -0400

    first commit

---
+++ Node.js/qrGeneratorNode.js
@@ -0,0 +1,20 @@
+const qrcode = require('qrcode');
+const express = require('express');
+
+const app = express();
+
+app.get('/v1/qr-code-generator', (req, res) => {
+    const url = 'https://hannahwroblewski.netlify.com/';
+
+    qrcode.toDataURL(url, (error, url) => {
+        if (error) {
+            res.status(500).json({ error: 'Failed to generate QR code'});
+        } else {
+            res.status(200).json({ url });
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
